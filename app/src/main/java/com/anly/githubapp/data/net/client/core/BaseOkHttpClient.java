0e74686e33e5bdfbd89bdd22ceb7243f4c9aa42e
zhouxingrong <zhouxingrong@putao.com>
2017-10-09 17:47:04 +0800

    ...

--- app/src/main/java/com/anly/githubapp/data/net/client/core/BaseOkHttpClient.java
+++ app/src/main/java/com/anly/githubapp/data/net/client/core/BaseOkHttpClient.java
@@ -1,7 +1,6 @@
 package com.anly.githubapp.data.net.client.core;
 
 import com.anly.githubapp.BuildConfig;
-import com.orhanobut.logger.Logger;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,12 +17,12 @@ public abstract class BaseOkHttpClient {
     public OkHttpClient get() {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.connectTimeout(TIMEOUT_CONNECT, TimeUnit.MILLISECONDS)
-                .addInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+                .addInterceptor(new HttpLoggingInterceptor(/*new HttpLoggingInterceptor.Logger() {
                     @Override
                     public void log(String message) {
                         Logger.d(message);
                     }
-                }).setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE));
+                }*/).setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE));
 
         builder = customize(builder);
 
